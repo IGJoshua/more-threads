a973f64f6cd726a7b1da7e0c8dd113581d4b85b2
Joshua Suskalo <joshua@suskalo.org>
2022-04-06 15:10:09 -0500

    Add initial utility functions and macros

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,8 @@
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
 ## [Unreleased]
+### Added
+- `if->` and `when->`, and paired thread-last versions to complement `cond->`
+- `when-pred`, `if-pred` and `cond-pred` functions for thread pipelines branching on the current value of the expression
 
 [Unreleased]: https://github.com/IGJoshua/more-threads/compare/a08cb7c68faa4b6d3bfb64c5129fcacc19a4efb4...HEAD
--- src/more_threads/core.clj
+++ src/more_threads/core.clj
@@ -1,1 +1,72 @@
 (ns more-threads.core)
+
+(defn when-pred
+  "Calls `then` on `x` if the result of calling `pred` on `x` is truthy.
+
+  This is useful for thread pipelines where [[cond->]] is not sufficient,
+  specifically when the condition depends on the current value flowing through
+  the threading pipeline."
+  [x pred then]
+  (if (pred x)
+    (then x)
+    x))
+
+(defn if-pred
+  "Calls either `then` or `else` on `x` depending on the truthiness of calling `pred` on `x`.
+
+  This is useful for thread pipelines where [[cond->]] is not sufficient,
+  specifically when the condition depends on the current value flowing through
+  the threading pipeline."
+  [x pred then else]
+  (if (pred x)
+    (then x)
+    (else x)))
+
+(defn cond-pred
+  "Returns the result of calling the then function on `x` from the first clause that returned truthy from calling its predicate on `x`.
+
+  `clauses` are of the form pred-fn then-fn where each is a function of `x`."
+  [x & clauses]
+  (some #(when ((first %) x)
+           ((second %) x))
+        (partition 2 clauses)))
+
+(defmacro when->
+  "Threads `x` through `then` as [[->]], but only if `test` is truthy.
+
+  If the `test` expression returns false, then `x` is returned unmodified.
+
+  This is equivalent to [[cond->]] with one clause."
+  [x test then]
+  `(let [x# ~x]
+     (if ~test
+       (-> x# ~then)
+       x#)))
+
+(defmacro if->
+  "Threads `x` through `then` or `else` as [[->]], depending on the truthiness of `test`."
+  [x test then else]
+  `(let [x# ~x]
+     (if ~test
+       (-> x# ~then)
+       (-> x# ~else))))
+
+(defmacro when->>
+  "Threads `x` through `then` as [[->>]], but only if `test` is truthy.
+
+  If the `test` expression returns false, then `x` is returned unmodified.
+
+  This is equivalent to [[cond->>]] with one clause."
+  [test then x]
+  `(let [x# ~x]
+     (if ~test
+       (->> x# ~then)
+       x#)))
+
+(defmacro if->>
+  "Threads `x` through `then` or `else` as [[->>]], depending on the truthiness of `test`."
+  [test then else x]
+  `(let [x# ~x]
+     (if ~test
+       (->> x# ~then)
+       (->> x# ~else))))
